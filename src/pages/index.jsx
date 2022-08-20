82e4b9ec1c9902a98de1913bfb3957648d8f56ae
Yoshi_3line <ny3line@gmail.com>
2022-08-20 16:44:40 +0900

    ⚡️ useEffectとuseCallbackの第二引数を追加しました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,26 +3,25 @@ import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
-import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+
+  const handleClick = useCallback(() => {
+    console.log(count);
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
+
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
 
-  const foo = 1;
-
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -32,9 +31,8 @@ export default function Home() {
 
       <Header />
 
-      <Link href="/about">
-        <a onClick={handleClick}>ボタン</a>
-      </Link>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 
