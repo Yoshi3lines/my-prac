d3e21f5ae7df42f2634ae5165c748e39251973e0
Yoshi_3line <ny3line@gmail.com>
2022-08-21 18:28:39 +0900

    ✨ コンポーネント間でのstateのリフトアップ

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,33 +1,9 @@
 import classes from "src/components/Links/Links.module.css";
 
-const ITEMS = [
-  {
-    href: "https://nextjs.org/docs",
-    title: "Documentation \u2192",
-    descrption: "Find in-depth information about Next.js features and API.",
-  },
-  {
-    href: "https://nextjs.org/learn",
-    title: "Learn \u2192",
-    descrption: "Learn about Next.js in an interactive course with quizzes!",
-  },
-  {
-    href: "https://github.com/vercel/next.js/tree/canary/examples",
-    title: "Examples \u2192",
-    descrption: "Discover and deploy boilerplate example Next.js projects.",
-  },
-  {
-    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Deploy \u2192",
-    descrption:
-      "Instantly deploy your Next.js site to a public URL with Vercel.",
-  },
-];
-
-export function Links() {
+export function Links({ items }) {
   return (
     <div className={classes.grid}>
-      {ITEMS.map((item) => {
+      {items.map((item) => {
         return (
           <a key={item.href} href={item.href} className={classes.card}>
             <h2>{item.title}</h2>
